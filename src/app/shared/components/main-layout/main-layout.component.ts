738a6651450164e92776b324077bdec4960b3bb8
Oleksandr Kolomiichuk <alexkolom27@gmail.com>
2020-09-06 22:34:13 +0200

    Update navbar

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,18 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    HostListener,
+    ViewChild,
+    ElementRef,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { ImageSource } from '../../types';
 
 @Component({
-  selector: 'app-main-layout',
-  templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+    selector: 'app-main-layout',
+    templateUrl: './main-layout.component.html',
+    styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
-    isLoggedIn: boolean
+    @ViewChild('navbar') navbarRef: ElementRef;
+    isNavbarFixed = false;
+    lastScrollTop = 0;
+    showProfileMenu = false;
+    userAvatarSrc: Observable<ImageSource>;
 
-    constructor(private auth: AuthService) {
-        this.auth.showProfile$.subscribe(arg => this.isLoggedIn = arg);
-    }
+    constructor(
+        public auth: AuthService,
+        private router: Router,
+        private user: UserService
+    ) {}
 
     ngOnInit(): void {
+        this.userAvatarSrc = this.user.userData$.pipe(
+            map((user) =>
+                user && user.avatar
+                    ? user.avatar
+                    : '../../../../assets/avatar.jpg'
+            )
+        );
+    }
+
+    handleProfileMenu(event?: MouseEvent) {
+        this.showProfileMenu = !this.showProfileMenu;
+    }
+
+    logOut() {
+        this.auth.logout();
+        this.router.navigate(['/authentication']);
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll(event: Event) {
+        const navbarHeight = this.navbarRef.nativeElement.clientHeight;
+        const newScrollTop = event.target['scrollingElement'].scrollTop;
+
+        if (newScrollTop > this.lastScrollTop) {
+            this.navbarRef.nativeElement.style.top = -navbarHeight + 'px';
+        } else {
+            this.navbarRef.nativeElement.style.top = '0px';
+        }
+
+        this.lastScrollTop = newScrollTop;
     }
 }
--- src/app/user-panel/shared/components/user-panel-layout/user-panel-layout.component.ts
+++ src/app/user-panel/shared/components/user-panel-layout/user-panel-layout.component.ts
@@ -1,23 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/shared/services/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-panel-layout',
   templateUrl: './user-panel-layout.component.html',
   styleUrls: ['./user-panel-layout.component.scss']
 })
-export class UserPanelLayoutComponent implements OnInit {
-
-    constructor(
-        private auth: AuthService,
-        private router: Router
-    ) {}
-
-    ngOnInit(): void {}
-
-    onLogOut() {
-        this.auth.logout()
-        this.router.navigate(['/authentication'])
-    }
-}
+export class UserPanelLayoutComponent {}

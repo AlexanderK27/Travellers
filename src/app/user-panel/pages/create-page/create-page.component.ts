cbf612fc3416abea8c6c45434fdc12e6294f550a
Oleksandr Kolomiichuk <alexkolom27@gmail.com>
2020-09-16 05:24:14 +0200

    Style create page

--- src/app/user-panel/pages/create-page/create-page.component.ts
+++ src/app/user-panel/pages/create-page/create-page.component.ts
@@ -6,11 +6,12 @@ import { PlanCard, Filters } from 'src/app/shared/interfaces';
 import { AlertService } from 'src/app/shared/services/alert.service';
 import { UserService } from 'src/app/shared/services/user.service';
 import { PublicationService } from 'src/app/shared/services/publication.service';
+import { ImagePickerService } from 'src/app/shared/components/img-picker/image-picker.service';
 
 @Component({
     selector: 'app-create-page',
     templateUrl: './create-page.component.html',
-    styleUrls: ['./create-page.component.scss']
+    styleUrls: ['./create-page.component.scss'],
 })
 export class CreatePageComponent implements OnInit {
     filterValues: Filters = {
@@ -21,108 +22,127 @@ export class CreatePageComponent implements OnInit {
         continent: '',
         country: '',
         duration: '',
-        people: ''
-    }
-    form: FormGroup
-    planView: PlanCard = null
-    selectedFile = null
-    submitted = false
-    userSub: Subscription
+        people: '',
+    };
+    form: FormGroup;
+    planView: PlanCard = null;
+    selectedFile = null;
+    submitted = false;
+    userSub: Subscription;
     quillConfig = {
         toolbar: [
             ['bold', 'italic', 'underline', 'strike'],
-            [{ 'script': 'sub'}, { 'script': 'super' }],
-            [{ 'header': 1 }, { 'header': 2 }],
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'align': [] }, 'blockquote'],
-            [{ 'indent': '-1'}, { 'indent': '+1' }],
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-            [{ 'size': ['small', false, 'large', 'huge'] }],
-            [{ 'color': [] }, { 'background': [] }],
-            ['link', 'image']
-        ]
-    }
-    quillStyle = {minHeight: '300px'}
+            [{ script: 'sub' }, { script: 'super' }],
+            [
+                { list: 'ordered' },
+                { list: 'bullet' },
+                { align: [] },
+                'blockquote',
+            ],
+            [{ indent: '-1' }, { indent: '+1' }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ size: ['small', false, 'large', 'huge'] }],
+            [{ color: [] }, { background: [] }, 'link', 'image'],
+        ],
+    };
+    quillStyle = { minHeight: '500px', padding: '10px 16px' };
 
     constructor(
         private alert: AlertService,
         private publications: PublicationService,
+        public pickerService: ImagePickerService,
         private router: Router,
         private userService: UserService
     ) {}
 
     ngOnInit(): void {
-        this.userSub = this.userService.userData$.subscribe(user => {
+        this.userSub = this.userService.userData$.subscribe((user) => {
             if (user) {
                 this.planView = {
                     author: this.userService.user.username,
                     authorId: this.userService.user.userId,
                     authorAv: this.userService.user.minAvatar,
-                    poster: '../../../../assets/avatar.jpg',
-                    title: 'Week in France'
-                }
+                    poster: '',
+                    title: 'My first title',
+                    created: new Date(),
+                };
             }
-        })
+        });
         this.form = new FormGroup({
-            title: new FormControl('Week in France', [Validators.required, Validators.maxLength(150)]),
-            text: new FormControl('', [Validators.required])
-        })
+            title: new FormControl('', [
+                Validators.required,
+                Validators.maxLength(100),
+            ]),
+            text: new FormControl('', [Validators.required]),
+        });
     }
 
     onSubmit() {
         if (this.form.invalid) {
-            return
+            return;
         }
 
-        this.submitted = true
+        this.submitted = true;
 
         const publication = {
             published: false,
             created: new Date(),
             filters: {
                 ...this.filterValues,
-                city: !this.filterValues.city ? '' : this.filterValues.city.trim().toLowerCase()
+                city: !this.filterValues.city
+                    ? ''
+                    : this.filterValues.city.trim().toLowerCase(),
             },
             ...this.planView,
-            text: this.form.value.text
-        }
+            poster: this.pickerService.croppedImagesSrc[0] || '',
+            text: this.form.value.text,
+        };
 
-        if (publication.poster === '../../../../assets/avatar.jpg') {
-            publication.poster = ''
-        }
-
-        this.publications.createPublication(publication).subscribe(() => {
-            this.userService.userData$.next({
-                ...this.userService.user,
-                publications: this.userService.user.publications + 1
-            })
-            this.alert.success('Your publication has been saved')
-            this.router.navigate(['/profile'])
-            this.submitted = false
-        }, (e) => {
-            this.alert.warning(`Unknown error: ${e}`)
-            this.submitted = false
-        })
+        this.publications.createPublication(publication).subscribe(
+            () => {
+                this.userService.userData$.next({
+                    ...this.userService.user,
+                    publications: this.userService.user.publications + 1,
+                });
+                this.alert.success('Your publication has been saved');
+                this.router.navigate(['/profile']);
+                this.submitted = false;
+            },
+            (e) => {
+                this.alert.warning(`Unknown error: ${e}`);
+                this.submitted = false;
+            }
+        );
     }
 
-    setFilterValues(values: Filters) {this.filterValues = {...values}}
+    setFilterValues(values: Filters) {
+        this.filterValues = { ...values };
+    }
 
-    setPlanViewTitle() {this.planView.title = this.form.value.title}
+    setPlanViewTitle() {
+        this.planView.title = this.form.value.title;
+    }
 
     uploadFile(event) {
-        this.selectedFile = event.target.files[0]
+        this.selectedFile = event.target.files[0];
 
         if (!this.selectedFile) {
-            return
+            return;
         }
 
-        if (this.selectedFile.type !== 'image/png' && this.selectedFile.type !== 'image/jpeg') {
-            return this.alert.warning('Only .png, .jpg and .jpeg formats are supported')
+        if (
+            this.selectedFile.type !== 'image/png' &&
+            this.selectedFile.type !== 'image/jpeg'
+        ) {
+            return this.alert.warning(
+                'Only .png, .jpg and .jpeg formats are supported'
+            );
         }
 
-        const reader = new FileReader()
+        const reader = new FileReader();
         reader.readAsDataURL(this.selectedFile);
         reader.onload = () => {
-            this.planView.poster = reader.result
-        }
+            this.planView.poster = reader.result;
+        };
     }
 }
--- src/app/user-panel/shared/components/filters/filters.component.ts
+++ src/app/user-panel/shared/components/filters/filters.component.ts
@@ -1,35 +1,35 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Filters } from 'src/app/shared/interfaces';
-import { countries } from 'src/app/shared/db'
+import { searchSelects } from 'src/app/shared/db';
 
 @Component({
     selector: 'app-filters',
     templateUrl: './filters.component.html',
-    styleUrls: ['./filters.component.scss']
+    styleUrls: ['./filters.component.scss'],
 })
 export class FiltersComponent implements OnInit {
-    @Input() values: Filters
-    @Output() onSelect: EventEmitter<any> = new EventEmitter<any>()
-    countries = countries
-    form: FormGroup
+    @Input() values: Filters;
+    @Output() onSelect: EventEmitter<any> = new EventEmitter<any>();
+    searchSelects = searchSelects;
+    form: FormGroup;
 
     constructor() {}
 
     ngOnInit(): void {
         this.form = new FormGroup({
-            'continent': new FormControl(this.values.continent),
-            'country': new FormControl(this.values.country),
-            'city': new FormControl(this.values.city, [Validators.maxLength(24)]),
-            'duration': new FormControl(this.values.duration),
-            'amountCountries': new FormControl(this.values.amountCountries),
-            'amountCities': new FormControl(this.values.amountCities),
-            'people': new FormControl(this.values.people),
-            'budget': new FormControl(this.values.budget)
-        })
+            continent: new FormControl(this.values.continent),
+            country: new FormControl(this.values.country),
+            city: new FormControl(this.values.city, [Validators.maxLength(30)]),
+            duration: new FormControl(this.values.duration),
+            amountCountries: new FormControl(this.values.amountCountries),
+            amountCities: new FormControl(this.values.amountCities),
+            people: new FormControl(this.values.people),
+            budget: new FormControl(this.values.budget),
+        });
 
-        this.form.valueChanges.subscribe(value => {
-            this.onSelect.emit(value)
-        })
+        this.form.valueChanges.subscribe((value) => {
+            this.onSelect.emit(value);
+        });
     }
 }

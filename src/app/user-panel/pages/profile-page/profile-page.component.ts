4941162a8104663d3ca95b6b28b448dfd3b1362d
Oleksandr Kolomiichuk <alexkolom27@gmail.com>
2020-09-16 09:43:13 +0200

    Style profile page

--- src/app/user-panel/pages/profile-page/profile-page.component.ts
+++ src/app/user-panel/pages/profile-page/profile-page.component.ts
@@ -7,14 +7,13 @@ import { PublicationService } from 'src/app/shared/services/publication.service'
 @Component({
     selector: 'app-profile-page',
     templateUrl: './profile-page.component.html',
-    styleUrls: ['./profile-page.component.scss']
+    styleUrls: ['./profile-page.component.scss'],
 })
 export class ProfilePageComponent implements OnInit, OnDestroy {
-    loading = true
-    publications: Array<Publication> = []
-    pubSub: Subscription
-    user: UserData
-    userSub: Subscription
+    loading = true;
+    publications: Array<Publication> = [];
+    user: UserData;
+    userSub: Subscription;
 
     constructor(
         private userService: UserService,
@@ -22,32 +21,41 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
     ) {}
 
     ngOnInit(): void {
-        this.pubSub = this.pubService.publications$.subscribe(publications => {
-            this.publications = publications
-        })
-        this.userSub = this.userService.userData$.subscribe(user => {
+        this.userSub = this.userService.userData$.subscribe((user) => {
             if (user) {
-                this.user = user
+                this.user = user;
 
-                this.pubService.getPublications("authorId", user.userId)
-                    .subscribe((pubs: {publication: Publication}) => {
-                        const publications = Object.values(pubs)
+                this.pubService
+                    .getPublications('authorId', user.userId)
+                    .subscribe((pubs: { publication: Publication }) => {
+                        const publications = Object.values(pubs);
 
-                        publications.forEach(p => p.authorAv = user.minAvatar)
+                        publications.forEach(
+                            (p) => (p.authorAv = user.minAvatar)
+                        );
 
-                        this.pubService.publications$.next(publications.reverse())
-                        this.loading = false
-                    })
+                        this.publications = publications.sort((a, b) => {
+                            return (
+                                Date.parse(b.created.toString()) -
+                                Date.parse(a.created.toString())
+                            );
+                        });
+
+                        this.loading = false;
+                    });
             }
-        })
+        });
     }
 
-    onDeletePublication(id: string) {
-        this.publications = this.publications.filter(pub => pub.link !== id)
+    deletePublication(id: string) {
+        this.userService.userData$.next({
+            ...this.user,
+            publications: this.user.publications - 1,
+        });
+        this.publications = this.publications.filter((pub) => pub.link !== id);
     }
 
     ngOnDestroy() {
-        this.pubSub.unsubscribe()
-        this.userSub.unsubscribe()
+        this.userSub.unsubscribe();
     }
 }

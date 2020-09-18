98078edbbbbc5862163e47bbba4359b0e6f6abdb
Oleksandr Kolomiichuk <alexkolom27@gmail.com>
2020-09-18 19:19:29 +0200

    Style page with saved publications

--- src/app/pages/saved-page/saved-page.component.ts
+++ src/app/pages/saved-page/saved-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { map, switchMap, take } from 'rxjs/operators';
 import { Publication } from 'src/app/shared/interfaces';
 import { AvatarService } from 'src/app/shared/services/avatar.service';
 import { PublicationService } from 'src/app/shared/services/publication.service';
@@ -8,14 +9,12 @@ import { UserService } from 'src/app/shared/services/user.service';
 @Component({
     selector: 'app-saved-page',
     templateUrl: './saved-page.component.html',
-    styleUrls: ['./saved-page.component.scss']
+    styleUrls: ['./saved-page.component.scss'],
 })
 export class SavedPageComponent implements OnInit, OnDestroy {
-    aSub: Subscription
-    loading = true
-    publications: Array<Publication> = []
-    pubSub: Subscription
-    uSub: Subscription
+    loading = true;
+    publications: Array<Publication> = [];
+    uSub: Subscription;
 
     constructor(
         private avatarService: AvatarService,
@@ -24,34 +23,51 @@ export class SavedPageComponent implements OnInit, OnDestroy {
     ) {}
 
     ngOnInit(): void {
-        this.pubSub = this.pubService.publications$.subscribe(pubs => {
-            this.publications = pubs
-        })
-        this.uSub = this.userService.userData$.subscribe(user => {
-            if (user) {
-                if (user.saved && user.saved.length) {
-                    this.pubService.getSavedPublications(user.saved).subscribe(publications => {
-                        const usernames = publications.map(p => p.author)
+        this.uSub = this.userService.userData$
+            .pipe(take(2))
+            .subscribe((user) => {
+                if (user) {
+                    // check if user has saved publications and then stop loading
+                    if (user.saved && user.saved.length) {
+                        let publications = [];
 
-                        this.aSub = this.avatarService.getMinAvatars(usernames).subscribe(avatars => {
-                            publications.forEach(pub => {
-                                pub.authorAv = avatars.find(a => a.username === pub.author).avatar
-                            })
+                        this.pubService
+                            .getSavedPublications(user.saved)
+                            .pipe(
+                                // the array includes null if any publication has been deleted
+                                // filter to avoid it
+                                map((pubs) => pubs.filter((p) => !!p)),
+                                switchMap((pubs) => {
+                                    publications = [...pubs];
 
-                            this.pubService.publications$.next(publications)
-                            this.loading = false
-                        })
-                    })
-                } else {
-                    this.pubService.publications$.next([])
-                    this.loading = false
+                                    // get avatars of each author
+                                    const usernames = pubs.map((p) => p.author);
+                                    return this.avatarService.getMinAvatars(
+                                        usernames
+                                    );
+                                }),
+                                take(1)
+                            )
+                            .subscribe((avatars) => {
+                                // assign appropriate avatar to each publication
+                                this.publications = publications.map((p) => {
+                                    p.authorAv = avatars.find((a) => {
+                                        return a.username === p.author;
+                                    }).avatar;
+
+                                    return p;
+                                });
+
+                                this.loading = false;
+                            });
+                    } else {
+                        this.loading = false;
+                    }
                 }
-            }
-        })
+            });
     }
 
     ngOnDestroy(): void {
-        this.pubSub.unsubscribe()
-        this.uSub.unsubscribe()
+        this.uSub.unsubscribe();
     }
 }

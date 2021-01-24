44ffb3cb0e10bcfee14e67c54bfa4b3874678068
Alexander <alexkolom27@gmail.com>
2021-01-24 01:39:36 +0100

    style: add small naming changes in confirm window

--- src/app/shared/components/confirm-window/confirm-window.component.spec.ts
+++ src/app/shared/components/confirm-window/confirm-window.component.spec.ts
@@ -14,9 +14,9 @@ describe('ConfirmWindowComponent', () => {
         fixture = TestBed.createComponent(ConfirmWindowComponent);
         component = fixture.componentInstance;
         component.settings = {
-            callback: () => {},
-            confirmButtonTitle: 'Delete',
-            text: 'Are you sure you want to delete your account?',
+            confirmBtnText: 'Delete',
+            doOnConfirm: () => {},
+            message: 'Are you sure you want to delete your account?',
         };
     });
 
@@ -30,19 +30,19 @@ describe('ConfirmWindowComponent', () => {
         const de = fixture.debugElement.query(By.css('.window-message'));
         const el: HTMLElement = de.nativeElement;
 
-        expect(el.textContent).toEqual(component.settings.text);
+        expect(el.textContent).toEqual(component.settings.message);
     });
 
-    it('should emit onClose event when cancel method has been called', () => {
+    it('should emit close event when cancel method has been called', () => {
         let result = null;
-        component.onClose.subscribe(() => (result = 1));
+        component.close.subscribe(() => (result = 1));
 
         component.cancel();
         expect(result).toBe(1);
     });
 
     it('should call incomming callback if confirm button has been pressed', () => {
-        const spy = spyOn(component.settings, 'callback');
+        const spy = spyOn(component.settings, 'doOnConfirm');
         let btn = fixture.debugElement.query(By.css('.btn-danger'));
 
         btn.triggerEventHandler('click', null);
